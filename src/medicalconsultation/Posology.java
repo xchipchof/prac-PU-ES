818be73fd85ff0eb62e5c8213a4b9918832dc8b3
xchipchof <diegortandorra@gmail.com>
2025-12-14 19:04:15 +0100

    classes bàsiques medical consultation package
    S'han copiat les templates proporcionades per l'enunciat en el projecte,
    queda implementarles.

---
+++ src/medicalconsultation/Posology.java
@@ -0,0 +1,14 @@
+package medicalconsultation;
+
+public class Posology { // A class that represents the posology of a medicine
+
+    private float dose;
+
+    private float freq;
+
+    private FqUnit freqUnit;
+
+    public Posology (float d, float f, FqUnit u) { . . . } // Initializes attributes
+
+    // the getters and setters
+}
