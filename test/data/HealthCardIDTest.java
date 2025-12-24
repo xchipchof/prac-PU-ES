0d0aa1f01e80bb3515e348529c2f173c9c32e32d
xchipchof <diegortandorra@gmail.com>
2025-12-25 00:59:32 +0100

    tests HealthCardID dissenyats
    Dissenyats els testos de la classe HealthCardID previament al disseny de
    la classe amb les excepcions extra. (intent de TTD)

--- test/data/HealthCardIDTest.java
+++ test/data/HealthCardIDTest.java
@@ -1,5 +1,40 @@
 package data;
 
+import data.HealthCardID;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HealthCardIDTest {
+
+    private HealthCardID cip;
+
+    @Test
+    public void arribaNullAlConstructorTest(){
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new HealthCardID(null)
+
+        );
+
+    }
+    @Test
+    public void arribaNoAlfanumericAlConstructorTest(){
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new HealthCardID("\uD83D\uDE0B")
+        );
+
+    }
+    @Test
+    public void arribaFormatIncorrecteAlConstructorTest(){
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new HealthCardID("A32B4")
+        );
+
+    }
     
 }
