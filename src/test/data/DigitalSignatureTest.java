ac83688c5a1195566fd5e0603d82ad83f23ff01a
xchipchof <diegortandorra@gmail.com>
2025-12-25 23:09:27 +0100

    Dissenyats els testos de la classe DigitalSignature
    He fixat com a format que sigui una signatura de 64 bytes. Tambe
    comprovo que sigui realment inmutable ja que podria modificar el valor
    dintre de la classe utilitzant la referencia del argument passat al
    constructor, per lo que s'ha de comprovar que sigui realment inmutable.

--- src/test/data/DigitalSignatureTest.java
+++ src/test/data/DigitalSignatureTest.java
@@ -1,4 +1,56 @@
 package data;
 
+
+import data.exceptions.InvalidDigitalSignatureFormatException;
+import data.DigitalSignature;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DigitalSignatureTest {
+
+    @Test
+    public void nullAlContructorTest(){
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new DigitalSignature(null)
+        );
+    }
+
+    @Test
+    public void arrayInmutableTest(){
+        byte[] valorSignatura =  "z8N2uP5vR1qX9fbYwL4mK7jH3dA6sG0tQ9vC2xZ5nB8mJ1kL4pO7iU0yT3rE6wQ9".getBytes(StandardCharsets.UTF_8);
+
+        DigitalSignature signa = new DigitalSignature(valorSignatura);
+
+        valorSignatura[0] = (byte) '0';
+
+        assertNotEquals(valorSignatura, signa.getSignature());
+
+    }
+
+    @Test
+    public void formatIncorrecteTest(){
+        byte[] signaturaFormatIncorrecte = "".getBytes();
+
+        assertThrows(
+                InvalidDigitalSignatureFormatException.class,
+                () -> new DigitalSignature(signaturaFormatIncorrecte)
+        );
+    }
+
+    @Test
+    public void equalsTest(){
+        byte[] key1 = "z8N2uP5vR1qX9fbYwL4mK7jH3dA6sG0tQ9vC2xZ5nB8mJ1kL4pO7iU0yT3rE6wQ9".getBytes(StandardCharsets.UTF_8);
+        byte[] key2 = "z8N2uP5vR1qX9fbYwL4mK7jH3dA6sG0tQ9vC2xZ5nB8mJ1kL4pO7iU0yT3rE6wQ9".getBytes(StandardCharsets.UTF_8);
+
+        DigitalSignature o1 = new DigitalSignature(key1);
+        DigitalSignature o2 = new DigitalSignature(key2);
+
+        assertEquals(o1, o2);
+    }
+
 }
