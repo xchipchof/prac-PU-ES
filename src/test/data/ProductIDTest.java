e3a90a493e414ba6370374578926daa7ee0e2a05
xchipchof <diegortandorra@gmail.com>
2025-12-25 21:45:14 +0100

    Restructuracio del directori del projecte
    Millor estructura d'acord a l'estandard.

---
+++ src/test/data/ProductIDTest.java
@@ -0,0 +1,43 @@
+package data;
+
+import data.ProductID;
+import data.exceptions.NonUPCFormatException;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductIDTest {
+
+    @Test
+    public void checkConstructor(){
+        ProductID idProducte = new ProductID("979543956330");
+
+        assertEquals("979543956330", idProducte.getUPC());
+    }
+
+    @Test
+    public void noCompleixFormatUPCTest(){
+        String codiLleig = "1234ABC";
+        assertThrows(
+                NonUPCFormatException.class,
+                () -> new ProductID(codiLleig)
+        ); 
+        }
+
+    @Test
+    public void llargadaIncorrectaDelCodiTest(){
+        String codiCurt = "1495038";
+        assertThrows(NonUPCFormatException.class,
+                () -> new ProductID(codiCurt)
+        );
+    }
+
+    @Test
+    public void codiNullTest(){
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new ProductID(null)
+        );
+    }
+
+}
